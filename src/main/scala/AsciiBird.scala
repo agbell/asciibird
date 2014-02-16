a6c3ed0da7a8f451c48932997f1b60dc6a55f1ed
Jeffrey Olchovy <olchovy@gmail.com>
2014-02-15 23:08:14 -0500

    use Try to parse cli args

--- src/main/scala/AsciiBird.scala
+++ src/main/scala/AsciiBird.scala
@@ -1,29 +1,17 @@
 import domain.Game
 
+import scala.util.Try
+
 object AsciiBird {
 
   def main(args: Array[String]) {
-
-    def parseArg[A](i: Int, default: A)(f: String => A): A = (try {
-      args.lift(i).map(f)
-    } catch {
-      case e: Exception => None
-    }).getOrElse(default)
-
-    val difficulty  = parseArg[Game.Level](0, default = Game.Levels.Normal) { arg =>
-      Game.Levels.valueOf(arg.toInt)
-    }
-
-    val width = parseArg(1, default = Game.MinWidth)(_.toInt)
-
-    val height = parseArg(2, default = Game.MinHeight)(_.toInt)
-
+    val difficulty = Try(args(0).toInt).map(Game.Levels.valueOf).getOrElse(Game.Levels.Normal)
+    val width = Try(args(1).toInt).getOrElse(Game.MinWidth)
+    val height = Try(args(2).toInt).getOrElse(Game.MinHeight)
     val game = Game(width, height, difficulty)
-
     sys.addShutdownHook {
       game.shutdown()
     }
-
     game.start()
   }
 }
